be114a98f797456e655ede21ad5b9af77f5e5453
Rebecca Troy <20088744@mail.wit.ie>
2022-12-27 16:30:16 +0000

    add basic movie api tests
    This commit begins to add tests to the /movies/ API
    calls. It tests basic functionality without yet
    checking MongoDB.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- movies-api/tests/functional/api/movies/index.js
+++ movies-api/tests/functional/api/movies/index.js
@@ -1,7 +1,8 @@
 import chai from "chai";
 import request from "supertest";
 const mongoose = require("mongoose");
-import Movie from "../../../../api/movies/movieModel";
+import MovieSchema from "../../../../api/movies/movieModel";
+import movieDetailsModel from "../../../../api/movies/movieDetailsModel";
 import api from "../../../../index";
 import movies from "../../../../seedData/movies";
 
@@ -9,12 +10,29 @@ const expect = chai.expect;
 let db;
 
 describe("Movies endpoint", () => {
-  before(() => {
+  before(async () => {
     mongoose.connect(process.env.MONGO_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     db = mongoose.connection;
+
+    //Define the different types of movie models
+    const movieModel = mongoose.model('Movies', MovieSchema);
+    const upcomingMovieModel = mongoose.model('UpcomingMovie', MovieSchema);
+    const trendingWeekMovieModel = mongoose.model('WeeklyTrendingMovie', MovieSchema);
+    const trendingDayMovieModel = mongoose.model('DailyTrendingMovie', MovieSchema);
+
+    //Remove any data stored in the DB for each collection
+    try {
+      await movieModel.deleteMany();
+      await upcomingMovieModel.deleteMany();
+      await trendingWeekMovieModel.deleteMany();
+      await trendingDayMovieModel.deleteMany();
+      await movieDetailsModel.deleteMany();
+    } catch (err) {
+      console.error(`failed to delete movie data: ${err}`);
+    }
   });
 
   after(async () => {
@@ -25,27 +43,65 @@ describe("Movies endpoint", () => {
     }
   });
 
-  beforeEach(async () => {
-    try {
-      await Movie.deleteMany();
-      await Movie.collection.insertMany(movies);
-    } catch (err) {
-      console.error(`failed to Load user Data: ${err}`);
-    }
-  });
   afterEach(() => {
     api.close(); // Release PORT 8080
   });
-  describe("GET /api/movies ", () => {
+
+  describe("GET /api/movies/discover", () => {
     it("should return 20 movies and a status 200", (done) => {
       request(api)
-        .get("/api/movies")
+        .get("/api/movies/discover")
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(200)
         .end((err, res) => {
-          expect(res.body).to.be.a("array");
-          expect(res.body.length).to.equal(20);
+          expect(res.body.results).to.be.a("array");
+          expect(res.body.results.length).to.equal(20);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/movies/upcoming", () => {
+    it("should return 20 movies and a status 200", (done) => {
+      request(api)
+        .get("/api/movies/upcoming")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.results).to.be.a("array");
+          expect(res.body.results.length).to.equal(20);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/movies/trending/week", () => {
+    it("should return 20 movies and a status 200", (done) => {
+      request(api)
+        .get("/api/movies/trending/week")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.results).to.be.a("array");
+          expect(res.body.results.length).to.equal(20);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/movies/trending/today", () => {
+    it("should return 20 movies and a status 200", (done) => {
+      request(api)
+        .get("/api/movies/trending/today")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.results).to.be.a("array");
+          expect(res.body.results.length).to.equal(20);
           done();
         });
     });
@@ -53,27 +109,63 @@ describe("Movies endpoint", () => {
 
   describe("GET /api/movies/:id", () => {
     describe("when the id is valid", () => {
-      it("should return the matching movie", () => {
-        return request(api)
+      it("should return the matching movie", async () => {
+        const res = await request(api)
           .get(`/api/movies/${movies[0].id}`)
           .set("Accept", "application/json")
           .expect("Content-Type", /json/)
-          .expect(200)
-          .then((res) => {
-            expect(res.body).to.have.property("title", movies[0].title);
+          .expect(200);
+        expect(res.body).to.have.property("title", movies[0].title);
+      });
+    });
+
+    describe("when the id is invalid", () => {
+      it("should return an error message", () => {
+        return request(api)
+          .get("/api/movies/1234567899999")
+          .set("Accept", "application/json")
+          .expect(500)
+          .expect((res) => {
+            expect(res.text).to.match(/Hey!! You caught the error 👍👍\. Here's the details: /);
           });
       });
     });
+  });
+
+  describe("GET /api/movies/:id/images", () => {
+    describe("when the id is valid", () => {
+      it("should return the matching movie images", async () => {
+        const res = await request(api)
+          .get(`/api/movies/${movies[0].id}/images`)
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(200);
+        expect(res.body.backdrops).to.be.a("array");
+      });
+    });
+
     describe("when the id is invalid", () => {
-      it("should return the NOT found message", () => {
+      it("should return an error message", () => {
         return request(api)
-          .get("/api/movies/9999")
+          .get("/api/movies/1234567899999/images")
+          .set("Accept", "application/json")
+          .expect(500)
+          .expect((res) => {
+            expect(res.text).to.match(/Hey!! You caught the error 👍👍\. Here's the details: /);
+          });
+      });
+    });
+
+    describe("when the movie hasn't been called previously", () => {
+      it("should return an error message", async () => {
+        return request(api)
+          .get(`/api/movies/${movies[1].id}/images`)
           .set("Accept", "application/json")
           .expect("Content-Type", /json/)
           .expect(404)
           .expect({
-            status_code: 404,
-            message: "The resource you requested could not be found.",
+            message: 'The resource you requested could not be found.',
+            status_code: 404
           });
       });
     });
