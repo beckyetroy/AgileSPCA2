00534b63a05ca891e0a56dbc72c2300a260c0298
Rebecca Troy <20088744@mail.wit.ie>
2022-12-27 23:01:36 +0000

    add basic user api tests
    This commit begins to add tests for the Users endpoint
    in the movies API. It doesn't yet deal with must watch,
    favourites, or reviews.
    
    I.e., it tests the following endpoints:
    /api/users | GET | Get all users registered with the MongoDB
    /api/users | POST | Authenticate / validate username and password passed through and, if successful, log the user in and generate JWT token
    /api/users?action=register | POST | Authenticate / validate username and password passed through and, if successful, register the user with the MongoDB
    /api/users/{username} | PUT | Update the user's password if valid and update the MongoDB accordingly
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- movies-api/tests/functional/api/users/index.js
+++ movies-api/tests/functional/api/users/index.js
@@ -6,104 +6,393 @@ import api from "../../../../index";
 
 const expect = chai.expect;
 let db;
-let user1token;
 
-// describe("Users endpoint", () => {
-//   before(() => {
-//     mongoose.connect(process.env.MONGO_DB, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     db = mongoose.connection;
-//   });
+describe("Users endpoint", () => {
+  before(async () => {
+    mongoose.connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = mongoose.connection;
 
-//   after(async () => {
-//     try {
-//       await db.dropDatabase();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-//   beforeEach(async () => {
-//     try {
-//       await User.deleteMany();
-//       // Register two users
-//       await request(api).post("/api/users?action=register").send({
-//         username: "user1",
-//         password: "test1",
-//       });
-//       await request(api).post("/api/users?action=register").send({
-//         username: "user2",
-//         password: "test2",
-//       });
-//     } catch (err) {
-//       console.error(`failed to Load user test Data: ${err}`);
-//     }
-//   });
-//   afterEach(() => {
-//     api.close();
-//   });
-//   describe("GET /api/users ", () => {
-//     it("should return the 2 users and a status 200", (done) => {
-//       request(api)
-//         .get("/api/users")
-//         .set("Accept", "application/json")
-//         .expect("Content-Type", /json/)
-//         .expect(200)
-//         .end((err, res) => {
-//           expect(res.body).to.be.a("array");
-//           expect(res.body.length).to.equal(2);
-//           let result = res.body.map((user) => user.username);
-//           expect(result).to.have.members(["user1", "user2"]);
-//           done();
-//         });
-//     });
-//   });
+    try {
+        await User.deleteMany();
+    } catch (err) {
+    console.error(`failed to delete user data: ${err}`);
+    }
+  });
 
-//   describe("POST /api/users ", () => {
-//     describe("For a register action", () => {
-//       describe("when the payload is correct", () => {
-//         it("should return a 201 status and the confirmation message", () => {
-//           return request(api)
-//             .post("/api/users?action=register")
-//             .send({
-//               username: "user3",
-//               password: "test3",
-//             })
-//             .expect(201)
-//             .expect({ msg: "Successful created new user.", code: 201 });
-//         });
-//         after(() => {
-//           return request(api)
-//             .get("/api/users")
-//             .set("Accept", "application/json")
-//             .expect("Content-Type", /json/)
-//             .expect(200)
-//             .then((res) => {
-//               expect(res.body.length).to.equal(3);
-//               const result = res.body.map((user) => user.username);
-//               expect(result).to.have.members(["user1", "user2", "user3"]);
-//             });
-//         });
-//       });
-//     });
-//     describe("For an authenticate action", () => {
-//       describe("when the payload is correct", () => {
-//         it("should return a 200 status and a generated token", () => {
-//           return request(api)
-//             .post("/api/users?action=authenticate")
-//             .send({
-//               username: "user1",
-//               password: "test1",
-//             })
-//             .expect(200)
-//             .then((res) => {
-//               expect(res.body.success).to.be.true;
-//               expect(res.body.token).to.not.be.undefined;
-//               user1token = res.body.token.substring(7);
-//             });
-//         });
-//       });
-//     });
-//   });
-// });
+  after(async () => {
+    try {
+      await db.dropDatabase();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  afterEach(() => {
+    api.close();
+  });
+
+  describe("POST /api/users ", () => {
+    describe("For registering", () => {
+        describe("when credentials are missing", () => {
+            before(() => {
+                return request(api)
+                .get("/api/users")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .then((res) => {
+                    expect(res.body.length).to.equal(0);
+                });
+            });
+            after(() => {
+                return request(api)
+                .get("/api/users")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .then((res) => {
+                    expect(res.body.length).to.equal(0);
+                });
+            });
+            it("should return a 401 status and error message when all details are missing", () => {
+                return request(api)
+                  .post("/api/users?action=register")
+                  .send({
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Please pass username and password.',
+                      success: false
+                  });
+              });
+            it("should return a 401 status and error message when password is missing", () => {
+              return request(api)
+                .post("/api/users?action=register")
+                .send({
+                  username: "testuser"
+                })
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401)
+                .expect({
+                    msg: 'Please pass username and password.',
+                    success: false
+                });
+            });
+            it("should return a 401 status and error message when username is missing", () => {
+                return request(api)
+                  .post("/api/users?action=register")
+                  .send({
+                    password: "TestPW123!"
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Please pass username and password.',
+                      success: false
+                  });
+              });
+        });
+      describe("when password is invalid", () => {
+        before(() => {
+            return request(api)
+            .get("/api/users")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body.length).to.equal(0);
+            });
+        });
+        after(() => {
+            return request(api)
+            .get("/api/users")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+                expect(res.body.length).to.equal(0);
+            });
+        });
+        it("should return a 401 status and error message", () => {
+          return request(api)
+            .post("/api/users?action=register")
+            .send({
+              username: "testuser",
+              password: "testpassword",
+            })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(401)
+            .expect({
+                msg: 'Sign up failed. Password invalid.',
+                code: 401
+            });
+        });
+      });
+      describe("when credentials are valid", () => {
+        after(() => {
+            return request(api)
+              .get("/api/users")
+              .set("Accept", "application/json")
+              .expect("Content-Type", /json/)
+              .expect(200)
+              .then((res) => {
+                expect(res.body.length).to.equal(1);
+                const result = res.body.map((user) => user.username);
+                expect(result).to.have.members(["testuser1"]);
+              });
+        });
+        it("should return a 201 status and the confirmation message", () => {
+          return request(api)
+            .post("/api/users?action=register")
+            .send({
+              username: "testuser1",
+              password: "TestPW123!",
+            })
+            .expect(201)
+            .expect({ msg: "Successfully created new user.", code: 201 });
+        });
+      });
+      describe("when username already exists", () => {
+        before(() => {
+            return request(api)
+            .get("/api/users")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+              expect(res.body.length).to.equal(1);
+              const result = res.body.map((user) => user.username);
+              expect(result).to.have.members(["testuser1"]);
+            });
+        });
+        after(() => {
+            return request(api)
+            .get("/api/users")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+              expect(res.body.length).to.equal(1);
+              const result = res.body.map((user) => user.username);
+              expect(result).to.have.members(["testuser1"]);
+            });
+        });
+        it("should return a 401 status and error message", () => {
+          return request(api)
+            .post("/api/users?action=register")
+            .send({
+              username: "testuser1",
+              password: "FaketestPW123!",
+            })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(401)
+            .expect({
+                msg: 'Sign up failed. Username already taken.',
+                success: false
+            });
+        });
+      });
+    });
+    describe("For logging in", () => {
+        describe("when credentials are missing", () => {
+            it("should return a 401 status and error message when all details are missing", () => {
+                return request(api)
+                  .post("/api/users")
+                  .send({
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Please pass username and password.',
+                      success: false
+                  });
+            });
+            it("should return a 401 status and error message when password is missing", () => {
+              return request(api)
+                .post("/api/users")
+                .send({
+                  username: "testuser1"
+                })
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401)
+                .expect({
+                    msg: 'Please pass username and password.',
+                    success: false
+                });
+            });
+            it("should return a 401 status and error message when username is missing", () => {
+                return request(api)
+                  .post("/api/users")
+                  .send({
+                    password: "TestPW123!"
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Please pass username and password.',
+                      success: false
+                  });
+            });
+        });
+        describe("when credentials are incorrect", () => {
+            it("should return a 401 status and error message when username is incorrect", () => {
+                return request(api)
+                  .post("/api/users")
+                  .send({
+                    username: "fakeusername",
+                    password: "TestPW123!"
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Authentication failed. User not found.',
+                      code: 401
+                  });
+            });
+            it("should return a 401 status and error message when password is incorrect", () => {
+                return request(api)
+                  .post("/api/users")
+                  .send({
+                    username: "testuser1",
+                    password: "FakePW123!"
+                  })
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(401)
+                  .expect({
+                      msg: 'Authentication failed. Wrong password.',
+                      code: 401
+                  });
+            });
+        });
+        describe("when credentials are correct", () => {
+            it("should return a 200 status and a generated token", () => {
+            return request(api)
+                .post("/api/users")
+                .send({
+                    username: "testuser1",
+                    password: "TestPW123!",
+                })
+                .expect(200)
+                .then((res) => {
+                expect(res.body.success).to.be.true;
+                expect(res.body.token).to.not.be.undefined;
+                });
+            });
+        });
+    });
+  });
+
+  describe("GET /api/users ", () => {
+    before(async () => {
+        return request(api)
+            .post("/api/users?action=register")
+            .send({
+                username: "testuser2",
+                password: "SecondTestPW123!"
+              })
+              .expect(201)
+              .expect({ msg: "Successfully created new user.", code: 201 });
+    });
+    it("should return the 2 added users and a status 200", (done) => {
+        request(api)
+        .get("/api/users")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.a("array");
+          expect(res.body.length).to.equal(2);
+          let result = res.body.map((user) => user.username);
+          expect(result).to.have.members(["testuser1", "testuser2"]);
+          done();
+        });
+    });
+  });
+
+  describe("PUT /api/users/:username ", () => {
+    before(async () => {
+        return request(api)
+            .get("/api/users")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .then((res) => {
+              const result = res.body.map((user) => user.username);
+              expect(result).includes("testuser1");
+            });
+    });
+    describe("when password is missing", () => {
+        it("should return a 404 status and error message", () => {
+          return request(api)
+            .put("/api/users/testuser1")
+            .send({
+            })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(404)
+            .expect({
+                msg: 'Unable to parse password',
+                code: 404
+            });
+        });
+    });
+    describe("when password is valid", () => {
+        it("should return a 200 status and confirmation message", () => {
+          return request(api)
+            .put("/api/users/testuser1")
+            .send({
+                password: "NewPW456?"
+            })
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .expect({
+                msg: 'User Updated Sucessfully',
+                code: 200
+            });
+        });
+        it("should fail log in with old password", () => {
+            return request(api)
+                .post("/api/users")
+                .send({
+                    username: "testuser1",
+                    password: "TestPW123!"
+                })
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(401)
+                .expect({
+                    msg: 'Authentication failed. Wrong password.',
+                    code: 401
+                });
+        });
+        it("should log in successfully with new password", () => {
+            return request(api)
+                .post("/api/users")
+                .send({
+                    username: "testuser1",
+                    password: "NewPW456?"
+                })
+                .expect(200)
+                .then((res) => {
+                expect(res.body.success).to.be.true;
+                expect(res.body.token).to.not.be.undefined;
+                });
+        });
+    });
+  });
+});
