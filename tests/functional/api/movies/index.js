6a8a63ae11323f1f638ae57945a176ec4031e004
Rebecca Troy <20088744@mail.wit.ie>
2022-12-28 01:03:50 +0000

    isolate movie test cases
    This commit isolates all of the test cases that have
    been added to the movies endpoint tests so far by
    ensuring the DB is cleared before each test case.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- tests/functional/api/movies/index.js
+++ tests/functional/api/movies/index.js
@@ -10,29 +10,13 @@ const expect = chai.expect;
 let db;
 
 describe("Movies endpoint", () => {
+
   before(async () => {
     mongoose.connect(process.env.MONGO_DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     db = mongoose.connection;
-
-    //Define the different types of movie models
-    const movieModel = mongoose.model('Movies', MovieSchema);
-    const upcomingMovieModel = mongoose.model('UpcomingMovie', MovieSchema);
-    const trendingWeekMovieModel = mongoose.model('WeeklyTrendingMovie', MovieSchema);
-    const trendingDayMovieModel = mongoose.model('DailyTrendingMovie', MovieSchema);
-
-    //Remove any data stored in the DB for each collection
-    try {
-      await movieModel.deleteMany();
-      await upcomingMovieModel.deleteMany();
-      await trendingWeekMovieModel.deleteMany();
-      await trendingDayMovieModel.deleteMany();
-      await movieDetailsModel.deleteMany();
-    } catch (err) {
-      console.error(`failed to delete movie data: ${err}`);
-    }
   });
 
   after(async () => {
@@ -48,6 +32,16 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/discover", () => {
+
+    beforeEach(async () => {
+      const movieModel = mongoose.model('Movies', MovieSchema);
+      try {
+        await movieModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete discover movie data: ${err}`);
+      }
+    });
+
     it("should return 20 movies and a status 200", (done) => {
       request(api)
         .get("/api/movies/discover")
@@ -63,6 +57,16 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/upcoming", () => {
+
+    beforeEach(async () => {
+      const upcomingMovieModel = mongoose.model('UpcomingMovie', MovieSchema);
+      try {
+        await upcomingMovieModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete upcoming movie data: ${err}`);
+      }
+    });
+
     it("should return 20 movies and a status 200", (done) => {
       request(api)
         .get("/api/movies/upcoming")
@@ -78,6 +82,16 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/trending/week", () => {
+
+    beforeEach(async () => {
+      const trendingWeekMovieModel = mongoose.model('WeeklyTrendingMovie', MovieSchema);
+      try {
+        await trendingWeekMovieModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete trending (weekly) movie data: ${err}`);
+      }
+    });
+
     it("should return 20 movies and a status 200", (done) => {
       request(api)
         .get("/api/movies/trending/week")
@@ -93,6 +107,16 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/trending/today", () => {
+
+    beforeEach(async () => {
+      const trendingDayMovieModel = mongoose.model('DailyTrendingMovie', MovieSchema);
+      try {
+        await trendingDayMovieModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete trending (daily) movie data: ${err}`);
+      }
+    });
+
     it("should return 20 movies and a status 200", (done) => {
       request(api)
         .get("/api/movies/trending/today")
@@ -108,7 +132,17 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/:id", () => {
+
+    beforeEach(async () => {
+      try {
+        await movieDetailsModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete movie details data: ${err}`);
+      }
+    });
+
     describe("when the id is valid", () => {
+
       it("should return the matching movie", async () => {
         const res = await request(api)
           .get(`/api/movies/${movies[0].id}`)
@@ -120,6 +154,7 @@ describe("Movies endpoint", () => {
     });
 
     describe("when the id is invalid", () => {
+
       it("should return an error message", () => {
         return request(api)
           .get("/api/movies/1234567899999")
@@ -133,33 +168,19 @@ describe("Movies endpoint", () => {
   });
 
   describe("GET /api/movies/:id/images", () => {
-    describe("when the id is valid", () => {
-      it("should return the matching movie images", async () => {
-        const res = await request(api)
-          .get(`/api/movies/${movies[0].id}/images`)
-          .set("Accept", "application/json")
-          .expect("Content-Type", /json/)
-          .expect(200);
-        expect(res.body.backdrops).to.be.a("array");
-      });
-    });
 
-    describe("when the id is invalid", () => {
-      it("should return an error message", () => {
-        return request(api)
-          .get("/api/movies/1234567899999/images")
-          .set("Accept", "application/json")
-          .expect(500)
-          .expect((res) => {
-            expect(res.text).to.match(/Hey!! You caught the error 👍👍\. Here's the details: /);
-          });
-      });
+    beforeEach(async () => {
+      try {
+        await movieDetailsModel.deleteMany();
+      } catch (err) {
+        console.error(`failed to delete movie details data: ${err}`);
+      }
     });
 
     describe("when the movie hasn't been called previously", () => {
       it("should return an error message", async () => {
         return request(api)
-          .get(`/api/movies/${movies[1].id}/images`)
+          .get(`/api/movies/${movies[0].id}/images`)
           .set("Accept", "application/json")
           .expect("Content-Type", /json/)
           .expect(404)
@@ -167,6 +188,45 @@ describe("Movies endpoint", () => {
             message: 'The resource you requested could not be found.',
             status_code: 404
           });
+      });
+    });
+
+    describe("when the movie has been called previously", () => {
+
+      beforeEach(() => {
+        return request(api)
+        .get(`/api/movies/${movies[0].id}`)
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .then((res) => {
+            expect(res.body).to.have.property("title", movies[0].title);
+          });
+      });
+
+      describe("when the id is valid", () => {
+
+        it("should return the matching movie images", async () => {
+          const res = await request(api)
+            .get(`/api/movies/${movies[0].id}/images`)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200);
+          expect(res.body.backdrops).to.be.a("array");
+        });
+      });
+
+      describe("when the id is invalid", () => {
+
+        it("should return an error message", () => {
+          return request(api)
+            .get("/api/movies/1234567899999/images")
+            .set("Accept", "application/json")
+            .expect(500)
+            .expect((res) => {
+              expect(res.text).to.match(/Hey!! You caught the error 👍👍\. Here's the details: /);
+            });
+        });
       });
     });
   });
