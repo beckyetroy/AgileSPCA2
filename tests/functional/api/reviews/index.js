517a70e47c6537e30fb61669d4b61c21f231239e
Rebecca Troy <20088744@mail.wit.ie>
2022-12-29 16:20:07 +0000

    add GET reviews route tests
    This commit adds the tests for the GET reviews
    route.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- tests/functional/api/reviews/index.js
+++ tests/functional/api/reviews/index.js
@@ -31,4 +31,105 @@ describe("Reviews endpoint", () => {
     afterEach(() => {
       api.close(); // Release PORT 8080
     });
+
+    describe("GET /api/reviews/:id", () => {
+
+        beforeEach(async () => {
+          try {
+            await movieDetailsModel.deleteMany();
+          } catch (err) {
+            console.error(`failed to delete movie details data: ${err}`);
+          }
+    
+          //Store output from the TMDB API call directly
+          //Using the 3rd seed data movie as reviews are not empty
+          tmdbReviews = await getMovieReviews(movies[2].id);
+        });
+    
+        describe("when the movie hasn't been called previously", () => {
+
+            it("should return an error message", async () => {
+              return request(api)
+                .get(`/api/reviews/${movies[2].id}`)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(404)
+                .expect({
+                  message: 'The resource you requested could not be found.',
+                  status_code: 404
+                });
+            });
+      
+            it("should not update the DB", async () => {
+              await request(api)
+                .get(`/api/reviews/${movies[2].id}`)
+                .set("Accept", "application/json");
+            
+              const foundMovie = await movieDetailsModel.findByMovieDBId(movies[2].id);
+              expect(foundMovie).to.be.null;
+            });
+          });
+      
+        describe("when the movie has been called previously", () => {
+      
+            beforeEach(async () => {
+              const res = await request(api)
+                .get(`/api/movies/${movies[2].id}`)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200);
+              expect(res.body).to.have.property("title", movies[2].title);
+            });
+      
+            describe("when the id is valid", () => {
+      
+              it("should return the matching movie reviews", async () => {
+                const res = await request(api)
+                  .get(`/api/reviews/${movies[2].id}`)
+                  .set("Accept", "application/json")
+                  .expect("Content-Type", /json/)
+                  .expect(200);
+                expect(res.body).to.be.a("array");
+              });
+      
+              it("should return the same movie reviews pulled from TMDB API", (done) => {
+                request(api)
+                  .get(`/api/reviews/${movies[2].id}`)
+                  .set("Accept", "application/json")
+                  .end((err, res) => {
+                    expect(res.body).to.deep.equal(tmdbReviews);
+                    done();
+                  });
+              });
+          
+              it("should update the DB", async () => {
+                await request(api)
+                  .get(`/api/reviews/${movies[2].id}`)
+                  .set("Accept", "application/json");
+              
+                // Confirm movie reviews pulled from the TMDB API is now in the DB
+                const foundMovie = await movieDetailsModel.findByMovieDBId(movies[2].id);
+                foundMovie.reviews.toObject().forEach((review, index) => {
+                  expect(review.id).to.deep.equal(tmdbReviews[index].id);
+                  expect(review.author).to.deep.equal(tmdbReviews[index].author);
+                  expect(review.content).to.deep.equal(tmdbReviews[index].content);
+                });
+              });
+            });
+      
+            describe("when the id is invalid", () => {
+      
+              it("should return an error message", () => {
+                return request(api)
+                  .get("/api/reviews/1234567899999")
+                  .set("Accept", "application/json")
+                  .expect(404)
+                  .expect({
+                    message: 'The resource you requested could not be found.',
+                    status_code: 404
+                  });
+              });
+            });
+        });
+    });
 });
