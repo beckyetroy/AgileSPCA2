a15ec79e79a53c0c9c4dee6f2a9208a79f924812
Rebecca Troy <20088744@mail.wit.ie>
2022-12-30 00:13:57 +0000

    add favourites tests
    This commit adds tests relating to the favourites
    feature of the user endpoint.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- tests/functional/api/users/index.js
+++ tests/functional/api/users/index.js
@@ -3,6 +3,7 @@ import request from "supertest";
 const mongoose = require("mongoose");
 import User from "../../../../api/users/userModel";
 import api from "../../../../index";
+import movies from "../../../../seedData/movies";
 
 const expect = chai.expect;
 let db;
@@ -499,6 +500,245 @@ describe("Users endpoint", () => {
             expect(res.body.success).to.be.true;
             expect(res.body.token).to.not.be.undefined;
           });
+      });
+    });
+  });
+
+  describe("GET /api/users/:username/favourites ", () => {
+
+    beforeEach(async () => {
+      try {
+        //Add a favourite
+        await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[0])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+      } catch (err) {
+        console.error(`failed to add favourite: ${err}`);
+      }
+    });
+
+    describe("When username is valid", () => {
+
+      it("should return the user's favourites and a status 200", async () => {
+        const res = await request(api)
+          .get("/api/users/testuser1/favourites")
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(200);
+        expect(res.body).to.be.a("array");
+        const foundMovie = res.body.some((movie) => movie.id === movies[0].id);
+        expect(foundMovie).to.be.true;
+      });
+
+      it("should show the same user favourites in the DB", async () => {
+        const res = await request(api)
+          .get("/api/users/testuser1/favourites")
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(200);
+
+        //Confirm favourite is stored in the DB
+        const foundUser = await User.findByUserName("testuser1");
+        const foundMovie = foundUser.favourites.some((movie) => movie.id === movies[0].id);
+        expect(foundMovie).to.be.true;
+      });
+    });
+
+    describe("When username is invalid", () => {
+
+      it("should return an error message and status 404", async () => {
+        await request(api)
+          .get("/api/users/testuser6/favourites")
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            msg: 'Unable to Get User Favourites',
+            code: 404
+          });
+      });
+    });
+  });
+
+  describe("POST /api/users/:username/favourites  ", () => {
+
+    describe("When movie is not already favourited", () => {
+
+      it("should return the user, including new favourite, and a 201 status", async () => {
+        const res = await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[1])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+        expect(res.body.username).to.deep.equal("testuser1");
+        const foundMovie1 = res.body.favourites.some((movie) => movie.id === movies[0].id);
+        expect(foundMovie1).to.be.true;
+        const foundMovie2 = res.body.favourites.some((movie) => movie.id === movies[1].id);
+        expect(foundMovie2).to.be.true;
+      });
+
+      it("should update the DB", async () => {
+        await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[1])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+
+        //Confirm both favourites are stored in the DB
+        const foundUser = await User.findByUserName("testuser1");
+        const foundMovie1 = foundUser.favourites.some((movie) => movie.id === movies[0].id);
+        expect(foundMovie1).to.be.true;
+        const foundMovie2 = foundUser.favourites.some((movie) => movie.id === movies[1].id);
+        expect(foundMovie2).to.be.true;
+      });
+    });
+
+    describe("When movie is already favourited", () => {
+
+      it("should return the user with no duplicate favourites and a 201 status", async () => {
+        const res = await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[0])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+        expect(res.body.username).to.deep.equal("testuser1");
+        const foundMovies = res.body.favourites.filter((movie) => movie.id === movies[0].id);
+        expect(foundMovies.length).to.equal(1);
+      });
+
+      it("should not update the DB", async () => {
+        await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[1])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+
+        //Confirm favourite is only stored once in the DB
+        const foundUser = await User.findByUserName("testuser1");
+        const foundMovies = foundUser.favourites.filter((movie) => movie.id === movies[0].id);
+        expect(foundMovies.length).to.equal(1);
+      });
+    });
+  });
+
+  describe("POST /api/users/:username/favourites?action=remove", () => {
+
+    beforeEach(async () => {
+      try {
+        //Add an additional favourite
+        await request(api)
+          .post("/api/users/testuser1/favourites")
+          .send(movies[2])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(201)
+      } catch (err) {
+        console.error(`failed to add favourite: ${err}`);
+      }
+    });
+
+    describe("When username is invalid", () => {
+
+      it("should return error message and 404 status", async () => {
+        await request(api)
+          .post("/api/users/testuser6/favourites?action=remove")
+          .send(movies[0])
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(404)
+          .expect({
+            msg: 'Unable to Remove Favourite',
+            code: 404
+          });
+      });
+    });
+
+    describe("When username is valid", () => {
+
+      describe("When movie is not favourited", () => {
+
+        it("should return the user's favourites, excluding removed, and a 200 status", async () => {
+          const res = await request(api)
+            .post("/api/users/testuser1/favourites?action=remove")
+            .send(movies[1])
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+          
+          //Confirm favourite set for removal hasn't been added
+          const foundMovies = res.body.filter((movie) => movie.id === movies[1].id);
+          expect(foundMovies.length).to.equal(0);
+
+          //Confirm it still shows the expected favourites
+          const foundMovies2 = res.body.filter((movie) => movie.id === movies[0].id);
+          expect(foundMovies2.length).to.equal(1);
+          const foundMovies3 = res.body.filter((movie) => movie.id === movies[2].id);
+          expect(foundMovies3.length).to.equal(1);
+        });
+
+        it("should not update the DB", async () => {
+          await request(api)
+            .post("/api/users/testuser1/favourites?action=remove")
+            .send(movies[1])
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+
+          //Confirm favourite set for removal hasn't been added
+          const foundUser = await User.findByUserName("testuser1");
+          const foundMovies = foundUser.favourites.filter((movie) => movie.id === movies[1].id);
+          expect(foundMovies.length).to.equal(0);
+
+          //Confirm it still shows the expected favourites
+          const foundMovies2 = foundUser.favourites.filter((movie) => movie.id === movies[0].id);
+          expect(foundMovies2.length).to.equal(1);
+          const foundMovies3 = foundUser.favourites.filter((movie) => movie.id === movies[2].id);
+          expect(foundMovies3.length).to.equal(1);
+        });
+      });
+
+      describe("When movie is favourited", () => {
+
+        it("should return the user favourites without the posted favourite and a 200 status", async () => {
+          const res = await request(api)
+            .post("/api/users/testuser1/favourites?action=remove")
+            .send(movies[0])
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+          //Confirm removed favourite isn't returned
+          const foundMovies = res.body.filter((movie) => movie.id === movies[0].id);
+          expect(foundMovies.length).to.equal(0);
+
+          //Confirm it still shows the expected favourite
+          const foundMovies2 = res.body.filter((movie) => movie.id === movies[2].id);
+          expect(foundMovies2.length).to.equal(1);
+        });
+
+        it("should update the DB", async () => {
+          await request(api)
+            .post("/api/users/testuser1/favourites?action=remove")
+            .send(movies[0])
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(200)
+
+          const foundUser = await User.findByUserName("testuser1");
+          //Confirm removed favourite isn't stored in the DB
+          const foundMovies = foundUser.favourites.filter((movie) => movie.id === movies[0].id);
+          expect(foundMovies.length).to.equal(0);
+
+          //Confirm expected favourite is still stored in the DB
+          const foundMovies2 = foundUser.favourites.filter((movie) => movie.id === movies[2].id);
+          expect(foundMovies2.length).to.equal(1);
+        });
       });
     });
   });
